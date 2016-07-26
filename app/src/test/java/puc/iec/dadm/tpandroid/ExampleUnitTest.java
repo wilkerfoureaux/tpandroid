bf3065019fcfbde3bc83fd59c296401487c0b48f
wilker.foureaux@gmail.com <wilker.foureaux@gmail.com>
2016-07-26 09:47:42 -0300

    Initial commit

---
+++ app/src/test/java/puc/iec/dadm/tpandroid/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package puc.iec.dadm.tpandroid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
